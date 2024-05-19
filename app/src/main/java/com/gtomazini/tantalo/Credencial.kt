751258aa109d831fb0c6b5c248427e9e32e40664
Gtomazini <gtomazini@hotmail.com>
2024-05-19 15:09:38 -0300

    Finalização de Splash e Credencial screen

---
+++ app/src/main/java/com/gtomazini/tantalo/Credencial.kt
@@ -0,0 +1,11 @@
+package com.gtomazini.tantalo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Credencial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_credencial)
+    }
+}
