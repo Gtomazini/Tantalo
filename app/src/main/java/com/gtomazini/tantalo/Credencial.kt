2bd7b48ad2285cb3d502df539e048d77ae45b09f
Gtomazini <gtomazini@hotmail.com>
2024-05-20 05:57:12 -0300

    Fazer lógica do login

--- app/src/main/java/com/gtomazini/tantalo/Credencial.kt
+++ app/src/main/java/com/gtomazini/tantalo/Credencial.kt
@@ -2,10 +2,17 @@ package com.gtomazini.tantalo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class Credencial : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credencial)
+
+
+
+
     }
+
 }
